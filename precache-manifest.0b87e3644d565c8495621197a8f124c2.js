eb5280e10a24352caf609636f1a1ea0378eb7cdb
Josephine Aguilar <josieamor@Josephines-MacBook-Pro.local>
2019-04-02 18:31:47 -0600

    Updates

---
+++ precache-manifest.0b87e3644d565c8495621197a8f124c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sailor-moon/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "950c09877b7e9d2ba30f",
+    "url": "/sailor-moon/static/js/runtime~main.49a1e149.js"
+  },
+  {
+    "revision": "936a7d702081ddad4087",
+    "url": "/sailor-moon/static/js/main.3e2f2878.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/sailor-moon/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "936a7d702081ddad4087",
+    "url": "/sailor-moon/static/css/main.c39ccf24.chunk.css"
+  },
+  {
+    "revision": "9160f54f356772e13952ec5cde971dba",
+    "url": "/sailor-moon/index.html"
+  }
+];
